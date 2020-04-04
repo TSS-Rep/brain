05e801752e4a0c7486f0e825482c34d99cb22ccf
Eduardo <eduardoluissd@gmail.com>
2020-04-03 23:35:32 -0600

    React setup

---
+++ brain/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/layout/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>welcome</h1>
+    </div>
+  );
+}
+
+export default App;
