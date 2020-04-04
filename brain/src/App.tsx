95ea6926be8e7b6f64153c665aefa23b32507a64
Eduardo <eduardoluissd@gmail.com>
2020-04-04 00:35:19 -0600

    Sign In and Up components

--- brain/src/App.tsx
+++ brain/src/App.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <h1>welcome</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
